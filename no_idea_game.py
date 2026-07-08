07182239adef8ab75db0ff5f073fe5c07b6e9e86
usep-arch <s93753564@gmail.com>
2026-07-08 18:03:55 +0600

    task: increase happiness or discrease by not duplicate or duplicate

---
+++ no_idea_game.py
@@ -0,0 +1,20 @@
+# no_idea_game.py
+
+def solve():
+    n, m = map(int, input().split())
+
+    arr = map(int, input().split())
+
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
+
+    happiness = 0
+
+    for num in arr:
+        if num in A:
+            happiness += 1
+        elif num in B:
+            happiness -= 1
+    print(happiness)
+
+solve()
